d090f92958f6fbcb7cb7dc8c579e9685fa983c62
jigalowvladimir@gmail.com <jigalowvladimir@gmail.com>
2025-05-25 18:09:00 +0300

    added: 06 lab + 05 lab

--- 05/lab05_project/shop/urls.py
+++ 05/lab05_project/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('products/', views.product_list, name='product_list'),
+    path('categories/', views.category_list, name='category_list'),
 ]
--- 05/lab05_project/shop/views.py
+++ 05/lab05_project/shop/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
-from .models import Product
+from django.shortcuts import render
+from .models import Product, Category
 
 def index(request):
     products = Product.objects.all()
     output = "\n".join(f"{p.name} - {p.price} $" for p in products)
     return HttpResponse(output, content_type="text/plain")
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'shop/product.html', {'products': products})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/category.html', {'categories': categories})
